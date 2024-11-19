e5e98484fd7bd16fea310deddf8f8c4f54298652
kwon-sun-min <152298591+kwon-sun-min@users.noreply.github.com>
2024-11-19 09:27:38 +0900

    2024-11-19

---
+++ Programmers/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution1 sol1 = new Solution1();
+        System.out.println(sol1.solution(10,5));
+        System.out.println(sol1.solution(7,2));
+    }
+}
