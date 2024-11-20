733eeca9ac76fbf4f27d157ea1e7aac36574e576
kwon-sun-min <kwsumin01@gmail.com>
2024-11-20 14:26:03 +0900

    2024-11-20

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Solution2 sol = new Solution2();
+        Solution3 sol = new Solution3();
         System.out.println(sol.solution(10,5));
         System.out.println(sol.solution(7,2));
     }
