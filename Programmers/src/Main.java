2e4cbaa99c9a00a5ebd278abcda91d5e781543be
kwon-sun-min <kwsumin01@gmail.com>
2024-11-24 11:38:30 +0900

    2024-11-24

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Solution7 sol = new Solution7();
-        System.out.println(sol.solution(50000, 50000));
-        System.out.println(sol.solution(-50000, -50000));
+        Solution8 sol = new Solution8();
+        System.out.println(sol.solution(3, 2));
+        System.out.println(sol.solution(7, 3));
+        System.out.println(sol.solution(1, 16));
+
     }
 }
---
+++ Programmers/src/Solution8.java
@@ -0,0 +1,8 @@
+class Solution8 {
+  public int solution(double num1, double num2) {
+      
+      double answer = num1 / num2 * 1000;
+
+      return (int)answer;
+  }
+}
