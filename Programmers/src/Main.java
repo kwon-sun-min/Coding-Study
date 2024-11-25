bd64165d0709b785eeb29cd4fcb7abb78a050159
kwon-sun-min <152298591+kwon-sun-min@users.noreply.github.com>
2024-11-25 17:20:57 +0900

    2024-11-25

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Solution8 sol = new Solution8();
-        System.out.println(sol.solution(3, 2));
-        System.out.println(sol.solution(7, 3));
-        System.out.println(sol.solution(1, 16));
+        Solution9 sol = new Solution9();
+        System.out.println(sol.solution(70));
+        System.out.println(sol.solution(91));
+        System.out.println(sol.solution(180));
 
     }
 }
