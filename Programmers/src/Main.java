57d9ae2f09f1f607ad522f21d960ed95c2269c73
kwon-sun-min <kwsumin01@gmail.com>
2024-12-02 01:42:42 +0900

    2024-12-02

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,22 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Solution15 sol = new Solution15();
-        
-        int[] num_li = {1, 2, 3, 4, 5};
+        Solution16 sol = new Solution16();
 
-        System.out.println(sol.solution(num_li));
+        int[] input = {1 ,2,3,4,5};
 
+        int[] output = sol.solution(input);
 
-        for (int i = 0; i < num_li.length; i++) 
-        {
-            System.out.println(sol.solution(num_li)[i]);
-            
-
+        for (int i = 0; i < output.length; i++) {
+            System.out.println(output[i]);
         }
-
-        
-
-
         
         
     }
