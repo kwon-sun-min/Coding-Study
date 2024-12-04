7eccdd5778afe4f1747c2e0983088cbe9103cf6e
kwon-sun-min <152298591+kwon-sun-min@users.noreply.github.com>
2024-12-05 08:28:54 +0900

    2024-12-05

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        Solution18 sol = new Solution18();
-        System.out.println(sol.solution(2, 10));
-        System.out.println(sol.solution(7, 15));
+        Solution19 sol = new Solution19();
 
+        int[] input1 = {1,2,3,4,5};
+        int[] output1 = sol.solution(input1, 1, 3);
 
+        for (int i = 0; i < output1.length; i++) 
+        {
+            System.out.println(output1[i]);
+        }
+
+        
+        int[] input2 = {1,3,5};
+        int[] output2 = sol.solution(input2, 1, 2);
+
+        for (int i = 0; i < output2.length; i++) 
+        {
+            System.out.println(output2[i]);
+        }
         
     }
 }
