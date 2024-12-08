5c8f42241f99646a0ef3721c8e18e3a89b3aff30
kwon-sun-min <kwsumin01@gmail.com>
2024-12-08 10:44:51 +0900

    2024-12-08

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Solution21 sol = new Solution21();
+        Solution22 sol = new Solution22();
 
         String input1 = "jaron";
-        String output1 = sol.solution(input1);
+        String output1 = sol.solution(input1, "j");
 
         System.out.println(output1);
 
         String input2 = "bread";
-        String output2 = sol.solution(input2);
+        String output2 = sol.solution(input2, "b");
 
         System.out.println(output2);
 
