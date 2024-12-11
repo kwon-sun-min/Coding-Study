f2b39aefc4359f6cd076a87100708c14fdd3fc05
kwon-sun-min <kwsumin01@gmail.com>
2024-12-11 14:43:07 +0900

    2024-12-11

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,9 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        Solution24 sol = new Solution24();
+        Solution25 sol = new Solution25();
 
-        System.out.println(sol.solution(1234));
-        System.out.println(sol.solution(930211));
+        String[] s1= {"a", "b", "c"};
+        String[] s2= {"com", "b", "d", "p", "c"};
+
+        int output = sol.solution(s1, s2);
+        System.out.println(output);
+
+        String[] s3= {"n", "omg"};
+        String[] s4= {"m", "dot"};
+        
+        System.out.println(sol.solution(s3, s4));
+
         
     }
 }
