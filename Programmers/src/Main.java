21a7354153ad7cf2fe6eaf4d690d6e18dd8e792d
kwon-sun-min <kwsumin01@gmail.com>
2024-12-16 22:06:35 +0900

    2024-12-16

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Solution29 sol = new Solution29();
+        Solution30 sol = new Solution30();
 
-        System.out.println(sol.solution(20));
-        System.out.println(sol.solution(100));
+        int[] input1 = {1,2,3,4,5};
+        System.out.println(sol.solution(input1));
 
+        int[] input2 = {0, 31, 24, 10, 1, 9};
+        System.out.println(sol.solution(input2));
 
     }
 }
