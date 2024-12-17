3695b62bec30d93fc1ddc265a2b49402ccec289a
kwon-sun-min <kwsumin01@gmail.com>
2024-12-17 11:38:33 +0900

    2024-12-17

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,12 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Solution30 sol = new Solution30();
+        Solution31 sol = new Solution31();
 
-        int[] input1 = {1,2,3,4,5};
-        System.out.println(sol.solution(input1));
+        System.out.println(sol.solution("bus"));
+        System.out.println(sol.solution("nice to meet you"));
+        System.out.println(sol.solution("buaa"));
 
-        int[] input2 = {0, 31, 24, 10, 1, 9};
-        System.out.println(sol.solution(input2));
 
     }
 }
---
+++ Programmers/src/Solution31.java
@@ -0,0 +1,13 @@
+class Solution31 {
+  public String solution(String my_string) {
+      my_string = my_string.replace("a", "");
+      my_string = my_string.replace("e", "");
+      my_string = my_string.replace("i", "");
+      my_string = my_string.replace("o", "");
+      my_string = my_string.replace("u", "");
+
+      String answer = my_string;
+
+      return answer;
+  }
+}
