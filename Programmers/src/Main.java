2324c0025e82e45af768bbc431c1bfa979b54d94
kwon-sun-min <kwsumin01@gmail.com>
2024-12-25 13:00:53 +0900

    2024-12-25

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,10 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        Solution41 sol = new Solution41();
+        Solution42 sol = new Solution42();
 
-        System.out.println(sol.solution(23));
-        System.out.println(sol.solution(51));
-        System.out.println(sol.solution(100));
+        int[] input1 = {3, 76, 24};
+        int[] output1 = sol.solution(input1);
+        for (int i = 0; i < output1.length; i++) 
+        {
+            System.out.println(output1[i]);
+        }
+
+        System.out.println();
+
+        int[] input2 = {1, 2, 3, 4, 5, 6, 7};
+        int[] output2 = sol.solution(input2);
+        for (int i = 0; i < output2.length; i++) 
+        {
+            System.out.println(output2[i]);
+        }
+
+        System.out.println();
+
+        int[] input3 = {30, 10, 23, 6, 100};
+        int[] output3 = sol.solution(input3);
+        for (int i = 0; i < output3.length; i++) 
+        {
+            System.out.println(output3[i]);
+        }
 
 
 
--- Programmers/src/Solution41.java
+++ Programmers/src/Solution41.java
@@ -3,7 +3,6 @@ public class Solution41 {
       String answer = "";
       String temp = "";
       String[] alphaTable =  {"a", "b","c", "d", "e", "f", "g", "h", "i", "j"};
-      String[] numTable = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
       while(age > 0)
       {
@@ -11,8 +10,6 @@ public class Solution41 {
         answer = temp + answer;
         age /= 10;
       }
-
-
 
       return answer;
   }
