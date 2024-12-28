49429d5342c36578fa2ac7fd136e27b9d6b462d5
kwon-sun-min <kwsumin01@gmail.com>
2024-12-29 00:25:44 +0900

    2024-12-29

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Solution46 sol = new Solution46();
+        Solution47 sol = new Solution47();
 
-        System.out.println(sol.solution(3, 2));
-        System.out.println(sol.solution(30, 15));
+        int[] input1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] input2 = {100, 95, 2, 4, 5, 6, 18, 33, 948};
+
+        int[][] output1 = sol.solution(input1, 2);
+        int[][] output2 = sol.solution(input2, 3);
+
+        System.out.println(Arrays.deepToString(output1));
+        System.out.println(Arrays.deepToString(output2));
+
+
 
 
     }
--- Programmers/src/Solution46.java
+++ Programmers/src/Solution46.java
@@ -1,11 +1,11 @@
 class Solution46 {
   public int solution(int balls, int share) {
-      long result = 1; // 중간 계산을 long으로 처리
+      long result = 1; 
 
       for (int i = 0; i < share; i++) {
           result = result * (balls - i) / (i + 1);
       }
 
-      return (int) result; // 최종 결과를 int로 변환하여 반환
+      return (int) result; 
   }
 }
