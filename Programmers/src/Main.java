54452c9b605fdf2005b55ae1d614e900afa802d0
2025-01-04 <@>
2025-01-04 18:36:02 +0900

    update

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        Solution52 sol = new Solution52();
-
-        System.out.println(sol.solution(3628800));
-        System.out.println(sol.solution(7));
+        Solution53 sol = new Solution53();
+        
+        String input1 = "cccCCC";
+        String input2 = "abCdEfghIJ";
+        
+        String output1 = sol.solution(input1);
+        String outpu2  = sol.solution(input2);
+        
+        System.out.println(output1);
+        System.out.println(output2);
 
 
         
