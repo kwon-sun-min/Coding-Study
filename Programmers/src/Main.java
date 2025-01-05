b08ccafcaf90c0402783e345eebc21b263d207de
kwon-sun-min <kwsumin01@gmail.com>
2025-01-05 20:40:22 +0900

    2025-01-05

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,17 +1,16 @@
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        Solution53 sol = new Solution53();
-        
-        String input1 = "cccCCC";
-        String input2 = "abCdEfgIJ";
-        
-        String output1 = sol.solution(input1);
-        String outpu2  = sol.solution(input2);
-        
-        System.out.println(output1);
-        System.out.println(output2);
-        
-    
-    }
+        Solution54 sol = new Solution54();
 
+        // LinkedList<Integer>를 받아서 문자열로 변환
+        LinkedList<Integer> result = sol.solution("hi12392");
+        String output = result.stream()
+                              .map(String::valueOf) // Integer를 String으로 변환
+                              .collect(Collectors.joining(", ")); // ", "로 연결
+
+        System.out.println(output); // 결과 출력
+    }
 }
---
+++ Programmers/src/Solution54.java
@@ -0,0 +1,22 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Solution54 {
+  public LinkedList<Integer> solution(String my_string) {
+      LinkedList<Integer> answer = new LinkedList<>();
+
+      for (int i = 0; i < my_string.length(); i++) 
+      {
+        if(Character.isDigit(my_string.charAt(i)))
+        {
+          answer.add(Character.getNumericValue(my_string.charAt(i)));
+        }
+      }
+
+      Collections.sort(answer);
+
+      return answer;
+  }
+   
+  
+}
