1735f3b1a74fb38edf0c9a2451d8ebbdd878e319
kwon-sun-min <kwsumin01@gmail.com>
2025-01-06 10:15:58 +0900

    2025-01-06

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,16 +1,12 @@
-import java.util.LinkedList;
-import java.util.stream.Collectors;
-
 public class Main {
     public static void main(String[] args) {
-        Solution54 sol = new Solution54();
+        Solution55 sol = new Solution55();
 
-        // LinkedList<Integer>를 받아서 문자열로 변환
-        LinkedList<Integer> result = sol.solution("hi12392");
-        String output = result.stream()
-                              .map(String::valueOf) // Integer를 String으로 변환
-                              .collect(Collectors.joining(", ")); // ", "로 연결
+        System.out.println(sol.solution("aAb1B2cC34oOp"));
+        System.out.println(sol.solution("1a2b3c4d123"));
 
-        System.out.println(output); // 결과 출력
+
+
+
     }
 }
