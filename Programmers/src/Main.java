8687c38b0c727ffeae9c19f4df58ffc89407739c
kwon-sun-min <kwsumin01@gmail.com>
2025-01-10 23:29:49 +0900

    2025-01-10

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,9 +1,8 @@
-import java.util.LinkedList;
-
 public class Main {
     public static void main(String[] args) {
-        Solution58 sol = new Solution58();
-        System.out.println(sol.solution("1 2 Z 3"));
+        Solution59 sol = new Solution59();
+        int[] input1 = {199, 72, 222};
+        System.out.println(sol.solution(input1));
 
     }
 }
