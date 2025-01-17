e400056bcd6cd733c7c4879165bbdbf72ab457b5
kwon-sun-min <kwsumin01@gmail.com>
2025-01-18 06:31:05 +0900

    2025-01-18

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Solution66 sol = new Solution66();
+        Solution67 sol = new Solution67();
         
-        int[] input1 = {4, 5, 6, 7, 8, 9, 10, 11, 12};
-        int[] input2 = {1, 9, 3, 10, 13, 5};
-        int[] input3 = {2, 100, 120, 600, 12, 12};
+        String input1 = "onetwothreefourfivesixseveneightnine";
+        String input2 = "onefourzerosixseven";
 
-        System.out.println(sol.solution(3, input1).toString());
-        System.out.println(sol.solution(3, input2).toString());
-        System.out.println(sol.solution(3, input3).toString());
+        System.out.println(sol.solution(input1));
+        System.out.println(sol.solution(input2));
+
+
 
 
     }
