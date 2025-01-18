9a74b36cd58b60714abf45e6cfc8b55efdff88ea
kwon-sun-min <kwsumin01@gmail.com>
2025-01-19 05:57:24 +0900

    2025-01-19

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,14 +1,9 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        Solution71 sol = new Solution71();
+        Solution72 sol = new Solution72();
 
-        int[] input1 = {1,8,3};
-        int[] input2 = {9, 10, 11, 8};
+        System.out.println(sol.solution("3 + 4"));
 
-        System.out.println(Arrays.toString(sol.solution(input1)));
-        System.out.println(Arrays.toString(sol.solution(input2)));
 
     }
 }
--- Programmers/src/Solution66.java
+++ Programmers/src/Solution66.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 
 class Solution66 {
   public LinkedList<Integer> solution(int n, int[] numlist) {
-      LinkedList<Integer> answer = new LinkedList();
+      LinkedList<Integer> answer = new LinkedList<Integer>();
       
       for (int i = 0; i < numlist.length; i++) 
       {
