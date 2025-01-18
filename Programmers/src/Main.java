695415b6136e3c1918e8f1ed641ef7f4e76bd45a
kwon-sun-min <kwsumin01@gmail.com>
2025-01-19 04:33:02 +0900

    2025-01-19

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,6 +1,14 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Solution70 sol = new Solution70();
-        System.out.println(sol.solution(24));
+        Solution71 sol = new Solution71();
+
+        int[] input1 = {1,8,3};
+        int[] input2 = {9, 10, 11, 8};
+
+        System.out.println(Arrays.toString(sol.solution(input1)));
+        System.out.println(Arrays.toString(sol.solution(input2)));
+
     }
 }
