45badcdc65f8e9a5f5abd0b5b731fbacd6ebe860
kwon-sun-min <kwsumin01@gmail.com>
2025-01-23 13:16:07 +0900

    2025-01-23

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -1,12 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Solution77 sol = new Solution77();
-        int[] input1 = {7, 77, 17};
-        int[] input2 = {10, 29};
-
-
-        System.out.println(sol.solution(input1));
-        System.out.println(sol.solution(input2));
+        Solution78 sol = new Solution78();
+        String input1 = "abc1Addfggg4556b";
+        String input2 = "abcdef123";
+        System.out.println(Arrays.toString(sol.solution(input1, 6)));
+        System.out.println(Arrays.toString(sol.solution(input2, 3)));
 
 
 
