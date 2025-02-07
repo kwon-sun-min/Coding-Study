317636149fa67c7ba50115d73e2f9ac63d44e9ac
kwonsunmin <kwsumin01@gmail.com>
2025-02-07 12:37:28 +0900

    .

---
+++ Programmers/src/Solution98.java
@@ -0,0 +1,32 @@
+class Solution98 {
+    public int solution(String A, String B) {
+        int answer = -1;
+
+        for (int i = 1; i <= A.length(); i++) 
+        {
+            //A = A.charAt(A.length()-i) + A.substring(0, A.length()-1);
+            char last = A.charAt(A.length()-1);
+            String remind = A.substring(0, A.length()-1);
+            A = last + remind;
+
+;            if(A.equals(B))
+            {
+                return i;
+            }
+
+            
+        }
+        
+        return answer;
+    }
+
+    public static void main(String[] args) 
+    {
+        Solution98 sol = new Solution98();
+        System.out.println(sol.solution("hello", "llohe"));
+        String temp = "hello";
+        temp = temp.charAt(temp.length()-1)+temp;
+        System.out.println(temp);
+
+    }
+}
