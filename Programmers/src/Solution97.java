724784a8a22ab5174873284fd5b557a05f6f194b
kwonsunmin <kwsumin01@gmail.com>
2025-02-07 11:48:49 +0900

    .

---
+++ Programmers/src/Solution97.java
@@ -0,0 +1,23 @@
+class Solution97 {
+    public int solution(int chicken) {
+        int service = 0;
+        int coupon = 0;
+        for (int i = 0; i < chicken; i++) 
+        {
+            coupon += 1;
+            if(coupon % 10 == 0)
+            {
+                coupon += 1;
+                service += 1;
+            }
+            
+        }
+        return service;
+    }
+
+    public static void main(String[] args) 
+    {
+        Solution97 sol = new Solution97();
+        System.out.println(sol.solution(1081));
+    }
+}
