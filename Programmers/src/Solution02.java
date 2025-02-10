30ca91f2a9bd759ef617360894401fd6d4763459
kwon-sun-min <kwsumin01@gmail.com>
2025-02-10 17:42:33 +0900

    .

---
+++ Programmers/src/Solution02.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+class Solution02 {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                return key;
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Solution02 sol = new Solution02();
+        
+        String[] participant1 = {"leo", "kiki", "eden"};
+        String[] completion1 = {"eden", "kiki"};
+        System.out.println(sol.solution(participant1, completion1)); 
+
+        String[] participant2 = {"marina", "josipa", "nikola", "vinko", "filipa"};
+        String[] completion2 = {"josipa", "filipa", "marina", "nikola"};
+        System.out.println(sol.solution(participant2, completion2)); 
+
+        String[] participant3 = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion3 = {"stanko", "ana", "mislav"};
+        System.out.println(sol.solution(participant3, completion3)); 
+  }
+}
