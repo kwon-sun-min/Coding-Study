d6352d4b9ab865728129e15e54e7ab772b079212
kwon-sun-min <kwsumin01@gmail.com>
2025-02-12 14:53:17 +0900

    .

--- Programmers/src/Solution05.java
+++ Programmers/src/Solution05.java
@@ -1,71 +1,62 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 class Solution05 {
-    public int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        return answer;
+    static class Song
+    {
+        int id;
+        int playCount;
+
+        public Song(int id, int playCount)
+        {
+            this.id = id;
+            this.playCount = playCount;
+        }
     }
 
-    public static void main(String[] args) {
-        HashMap<String, Integer> genres_view = new HashMap<>();
-        HashMap<Integer, Integer> Num_songs = new HashMap<>();
 
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
+    public int[] solution(String[] genres, int[] plays) {
 
+        HashMap<String, Integer> genre_count = new HashMap<>();
+        HashMap<String, List<Song>> genre_song = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) 
         {
-            genres_view.put(genres[i], genres_view.getOrDefault(genres[i], 0) + plays[i]);
+            genre_count.put(genres[i], genre_count.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-
-        for (int i = 0; i < plays.length; i++) 
+        for (int i = 0; i < genres.length; i++) 
         {
-            Num_songs.put(i, plays[i]);
+            genre_song.putIfAbsent(genres[i], new ArrayList<>());
+            genre_song.get(genres[i]).add(new Song(i, plays[i]));
         }
 
+        List<String> sortedGenres = new ArrayList<>(genre_count.keySet());
+        Collections.sort(sortedGenres, (a, b) -> genre_count.get(b) - genre_count.get(a));
 
-        int[] views = new int[genres_view.size()];
-        int index = 0;
+        List<Integer> bestAlbum = new ArrayList<>();
 
-        for(int view : genres_view.values())
-        {
-            views[index++] = view;
-        }
-        Arrays.sort(views);
+        for (String genre : sortedGenres) {
+            List<Song> songs = genre_song.get(genre);
 
-
-
-        for(String gene : genres_view.keySet())
-        {
-            for (int i = 0; i < views.length; i++) 
-            {
-                if(genres_view.get(gene) == views[i])
-                {
-                    
+            songs.sort((a, b) -> {
+                if (a.playCount == b.playCount) {
+                    return a.id - b.id;  // 고유 번호 오름차순
+                } else {
+                    return b.playCount - a.playCount;  // 재생 횟수 내림차순
                 }
-                
+            });
+
+            bestAlbum.add(songs.get(0).id);
+            if (songs.size() > 1) {
+                bestAlbum.add(songs.get(1).id);
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-            
-
-
+        // 7. 결과를 int 배열로 변환하여 반환
+        return bestAlbum.stream().mapToInt(i -> i).toArray();
     }
+
 }
