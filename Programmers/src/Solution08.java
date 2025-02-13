d36e9a75ad327406954fd3847107c5532fa97c6a
kwonsunmin <kwsumin01@gmail.com>
2025-02-14 01:05:35 +0900

    .

---
+++ Programmers/src/Solution08.java
@@ -0,0 +1,50 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+class Solution08 {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if(s.charAt(i) == '(')
+            {
+                stack.add(s.charAt(i));
+            }
+
+            else
+            {
+                if(!stack.isEmpty())
+                {
+                    stack.pop();
+                }
+
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        if(!stack.isEmpty())
+        {
+            return false;
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Solution08 sol = new Solution08();
+        System.out.println(sol.solution("()()"));
+        System.out.println(sol.solution("(())()"));
+        System.out.println(sol.solution("()("));
+        System.out.println(sol.solution("())"));
+
+
+
+
+    }
+}
